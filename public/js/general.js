71b1bc3f47b924f47ebfdf9124970804fa355dc8
SedemQuame <sedem.amekpewu.3@gmail.com>
2019-12-21 03:47:32 +0000

    simple changes

---
+++ public/js/general.js
@@ -0,0 +1,6 @@
+// jshint esversion:6
+
+const commentBox = require('commentbox.io');
+// or if using the CDN, it will be available as a global "commentBox" variable.
+
+commentBox('my-project-id');
